d683454d8d7124594b80c873f3f882434b7efd15
Behnam <shahriaribehnam@gmail.com>
2021-02-08 17:51:38 +0200

    Adding card component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return (
-        <div>
-            <img src="https://robohash.org/test?200x200" alt="Robot"/>
+        <div className={'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'}>
+            <img src={`https://robohash.org/${id}?200x200`} alt="Robot"/>
             <div>
-                <h2>Name</h2>
-                <p>Email</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
