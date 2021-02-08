63f4b0d49b95c6fdc5796476be99ee03b09a4093
Behnam <shahriaribehnam@gmail.com>
2021-02-08 17:31:47 +0200

    Adding main files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test?200x200" alt="Robot"/>
+            <div>
+                <h2>Name</h2>
+                <p>Email</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
