49e08d7fe7e1045f75a71cf186ebb8d3eb17ad1f
Behnam <shahriaribehnam@gmail.com>
2021-02-09 18:12:20 +0200

    Adding CardList Component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import CardList from './CardList';
 import {robots} from "./robots";
 
 ReactDOM.render(
     <React.StrictMode>
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+            <CardList robots={robots}/>
         </div>
     </React.StrictMode>,
     document.getElementById('root')
